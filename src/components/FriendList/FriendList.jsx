6fe624b2253ad84dee1f4ab106bba02957a621db
Redddeve <popko.dmitry797@gmail.com>
2023-09-06 18:22:23 +0300

    Initial commit

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function FriendList() {
+  return (
+    <ul class="friend-list">
+      <li class="item">
+        <span class="status"></span>
+        <img class="avatar" src="" alt="User avatar" width="48" />
+        <p class="name"></p>
+      </li>
+    </ul>
+  );
+}
+
+export default FriendList;
